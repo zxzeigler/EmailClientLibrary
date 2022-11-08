dace087c0147e9ab40c7d874797fe39c07eb36c4
Zpage <Zpage@PORT-STANLEY>
2022-11-08 06:52:42 -0500

    API in progress

--- EmailClientAPI/Api.cs
+++ EmailClientAPI/Api.cs
@@ -1,10 +1,51 @@
-﻿using EmailClientLibrary;
-
-
-namespace EmailClientAPI
+﻿namespace EmailClientAPI
 {
-    public class Api
+    public static class Api
     {
-        EmailClient emailclient = new EmailClient();
+        public static void ConfigureApi(this WebApplication app)
+        {
+            app.MapPut("/CreateEmail", CreateEmail);
+            app.MapPut("/UpdateRecipient", UpdateRecipient);
+            app.MapPost("SendEmail", SendEmail);
+        }
+
+        private static async Task<IResult> CreateEmail(EmailClient ec, string subject, string body, string recipient)
+        {
+            try
+            {
+                //await ec.CreateEmail(subject, body, recipient);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> UpdateRecipient(EmailClient ec, string recipient)
+        {
+            try
+            {
+                //await ec.UpdateRecipient(recipient);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> SendEmail(EmailClient ec)
+        {
+            try
+            {
+                //await ec.SendEmail();
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
     }
 }
